abd6c8e3c679c7794ec153a97e02df56bfeb4f39
Gunnar-Oeh <gunnaroehmichen@posteo.de>
2023-05-11 13:59:38 +0200

    commented 02_layout_plots.R

--- scripts/02_layout_plots.R
+++ scripts/02_layout_plots.R
@@ -1,10 +1,10 @@
 ##############################################################
 ##############################################################
-### Plots zeichnen
+### Draw Plots
 ##############################################################
 ##############################################################
 
-### Pakete laden
+### load Packages
 require (ggplot2)
 require (ggrepel)
 require (plyr)
@@ -25,35 +25,41 @@ require (ggmap)
 require (gridExtra)
 require (exifr)
 
-### Working Directory setzen
-setwd ("/home/gunnar/Dokumente/Reise/")
+### Set Working Directory 
+setwd ("~/Dokumente/IT_Projects/Bikepacking_Maps/")
 
+### Load Data saved after running 01_data_wrangling_feature_engineering.R
 load ("./data/script_4.RData")
 
+### Set ggplot theme
 theme_set (theme_bw())
 
-### Farben festlegen
+### Set colors
 single_blue <- "#0a1451"
 single_red <- "#a40000"
 
-### Anpassungen in gpx_sf
-gpx_sf$art_unterkunft <- as.factor (gpx_sf$art_unterkunft)
-levels (gpx_sf$art_unterkunft) <- c ("Camping", "Wildcamping", "WarmShowers", "CouchSurfing", "Zimmer")
-gpx_sf$distanz <- as.numeric (st_length (gpx_sf))
+### Changes to gpx_sf
+gpx_sf$art_unterkunft <- as.factor (gpx_sf$art_unterkunft) ### Acommodation to factor
+levels (gpx_sf$art_unterkunft) <- c ("Camping", "Wildcamping", "WarmShowers", "CouchSurfing", "Zimmer") # factorlevels
+gpx_sf$distanz <- as.numeric (st_length (gpx_sf)) ### From units to numeric
 
-### Funktion für eine um einen Teiler erweiterte bbox für get_map
-enl_bb <- function (df_sf, pad, which = c ("ggmap", "sf")) { ### Einen sf dataframe als zu verarbeitendes Objekt und Teiler 
-  ### welcher die Erweiterung der Bounding Box festlegt
+#########################################################
+### Enlarge the Bounding-Box of the Map 
+### in ggplot by a relative amount of the original map
+#########################################################
+
+### Input: sf dataframe as basis for the bbox and pad as the relative amount the bbox shall enlarged
+enl_bb <- function (df_sf, pad, which = c ("ggmap", "sf")) { 
   
   bbx <- st_bbox (df_sf)
   
   ### xmin + 1/8
   x_pad <- (bbx[3] - bbx[1])/pad
-  ### ymin + 1/8 des Extent
+  ### ymin + 1/8 of the extent
   y_pad <- (bbx[4] - bbx[2])/pad
   
   if (which == "ggmap"){ 
-    ### Erweiterte Numerische Bounding box erstellen
+    ### Create enlarged bbox
     x_lim <- c (bbx[1] - x_pad, bbx[3] + x_pad)
     y_lim <- c (bbx[2] - y_pad, bbx[4] + y_pad)
     
@@ -68,7 +74,7 @@ enl_bb <- function (df_sf, pad, which = c ("ggmap", "sf")) { ### Einen sf datafr
       return (bbx)
     } }
 
-### Funktion um Versatz auf größerer Skalierung zu verhindern
+### Function to prevent offset on larger scale
 ggmap_bbox <- function(map) {
   if (!inherits(map, "ggmap")) stop("map must be a ggmap object")
   # Extract the bounding box (in lat/lon) from the ggmap to a numeric vector, 
@@ -88,27 +94,28 @@ ggmap_bbox <- function(map) {
   map
 }
 
-#################################################
-### 1.Insetkarte - Wo ist der Streckenabschnitt
-#################################################
+####################################################
+### 1.Inset-Map - Where is the daily route located
+####################################################
 
-### Hintergrundkarte nur als leeres Polygon
+### Backgroundmap as empty polygon
 europe <- ne_countries(continent = "europe", returnclass = "sf")
 
-st_crs(europe) ### 4326 wie gpx sf
+st_crs(europe) ### In EPSG:4326 like gpx sf
 st_crs (gpx_sf)
 
-### Europa auf vergrößerter bbox um die Strecke beschneiden
+### Crop Europe on enlarged bbox around route
 bbm <- enl_bb(st_bbox(gpx_sf), 12, which = "sf")
 
 background <- st_crop(europe, bbm)
 
-### Eine Karte der für jede Etappe in der noch die Tagesroute hervorgehoben wird
+### Background map including the whole route in black, where the route of the day is highlighted
 inset_background <- ggplot () +
   geom_sf (data = background, aes (geometry = geometry), fill = "#98b2a1", alpha = 0.33) +
   geom_sf (data = gpx_sf, aes (geometry = geometry), colour = single_blue, size = 1) +
   theme_void()
 
+### Create List with the the inset maps for each day
 p_inset <- list ()
 for (i in 1:nrow (gpx_sf)) {
   x <- gpx_sf[i, ]
@@ -128,45 +135,44 @@ for (i in 1:nrow (gpx_sf)) {
   
   rm (x, point_df, i, xpol)}
 
-### Sieht gut aus!
-
 #################################################
-### 2. Karte der Strecke
+### 2. Zoomed in Map of the daily route
 #################################################
-p_map <-list ()
-for (i in 1:nrow (gpx_sf)) {
+p_map <-list () ### list of daily maps
+for (i in 1:nrow (gpx_sf)) { # for each day
   
   x <- gpx_sf[i,]
   
-  bbm <- enl_bb(x, 7, which = "ggmap")
-  ### backgroundmap with ggmaps herunterladen
+  bbm <- enl_bb(x, 7, which = "ggmap") ### Apply enlarging of bounding box of daily route
+  
+  ### Download backgroundmap with ggmaps
   terrain_map <- get_map(location = bbm)
   terrain_map <- ggmap_bbox (terrain_map)
   
-  ### Umwandeln in 3857 
+  ### Transform geodata to EPSG:3857 
   x <- st_transform(x, 3857)
-  ### In Punkte umwandeln um die Namen der STädte einblenden zu können
+  ### Cast to points, to display Names of start and stop towns
   stz_sf <- st_cast (x, "POINT")
-  ### Wie viele Punkte auf der Route = Index des letzten Punktes (Zielort)
+  ### Number o points = Index of the last point (stop) 
   n_xy <- nrow (stz_sf)
   
-  ### Nur Geometrie und Ortspalten auswählen
+  ### Use only geometry and location names
   stz_sf <- stz_sf[c(1,n_xy),c(11,4,5)]
-  ### Neue Spalte die Start und Ziel enthält
+  ### Single column forstart and stop town-names
   stz_sf$start_ziel <- c (stz_sf$start[1], stz_sf$ziel[2])
-  ### Beschränken aufKoordinaten und die Namensspalte
+  ### restrict to coordinates and start_stop column
   stz_sf <- stz_sf[,c(1,4)]
-  ### Als normalen DF für die geom_label_repel Funktion
+  ### Cast to base non-spatial df to use with gglabel and ggrepel
   stz_sf <- data.frame (st_coordinates(stz_sf), start_ziel = stz_sf$start_ziel)
   
-  ### Beschriftungen
+  ### Annotations (date, number of stage): 
   nam <- paste ("Etappe " , x$ID, " - ", format (x$date, "%d.%m."), sep = "")
-  ### Untertitel
+  ### Subtitles (accomodationm)
   cap <- paste (round (x$distanz/1000, 0), " km - ", x$art_unterkunft, 
                 if (x$art_unterkunft %in% c ("WarmShowers", "CouchSurfing")) {
                   paste (" bei ", x$name_gastgeberIn, sep = "")}, sep = "")
   
-  ### Eigentliche Route
+  ### Actual route of the day
   p_map[[i]] <- ggmap (terrain_map) +
     geom_label_repel (data = stz_sf, aes (x = X, y = Y, label = start_ziel))+
     geom_sf (data = x, aes (geometry = geometry), 
@@ -179,31 +185,35 @@ for (i in 1:nrow (gpx_sf)) {
 }
 
 ####################################
-### 3. Höhenprofil
+### 3. Display elevation profile
 ####################################
 
+# Elevation profiles into a list using dlply
 p_elev <- dlply(df_elevation, .(ID), function (x) {
   
-  ### Limits: 0, bzw.darunterliegender hunderterwert bis darüber liegender 
-  ### Hunderterwert
+  ### Set the limits of the elevation plot depending on the min and max of that day
   max_elev <- max (x$elevation, na.rm = TRUE)
   min_elev <- min (x$elevation, na.rm = TRUE)
   
+  ### Set the limits of the x axis (travelled distance) in km
   ylims <- c ( if (min_elev >= 0 ) {floor (min_elev/100)*100}
                else {floor (min_elev/10)*10},  ceiling(max_elev/100) * 100)
   xlim <- max (x$dist_cycle)/1000
   
   ID <- unique (x$ID)
   
+  ### Extract gained elevation data from gpx_sf
   IND <- which (gpx_sf$ID == ID)
   up <- round (gpx_sf$up[IND], 0)
   down <- round (gpx_sf$down[IND], 0)
   
+  ### Summary string
   cap <- paste ("Auf: ", up, "hm; Ab: ", down, "hm", sep = "")
   
   cap_xy <-  c (  xlim - xlim/2 ,
                   ylims[2] - ylims[2]/ 12 )
   
+  ### Create plot
   ggplot( data = x, aes (x = dist_cycle/1000)) +
     geom_ribbon(aes (ymin = ylims[1], 
                      ymax = rollmean (elevation, k = 9, fill = "extend")), 
@@ -214,13 +224,11 @@ p_elev <- dlply(df_elevation, .(ID), function (x) {
     annotate ("label", x = cap_xy[1], y = cap_xy[2], label = cap, alpha = 0.66)
 } ) 
 
-### läuft
+#############################################################
+### Add inlet, route and elevation plot into a single layout
+#############################################################
 
-####################################
-### Zusammenfügen
-####################################
-
-### Layout des Plotgitters
+### Layout of the Plotgrid
 lay <- rbind (c (NA, NA, 2, 2, 2 ), 
               c (NA, 1, 2, 2, 2), 
               c (NA, NA, 3, 3, 3))
@@ -228,20 +236,20 @@ lay <- rbind (c (NA, NA, 2, 2, 2 ),
 lay <- rbind ( c (1, 1,  2, NA),  
                c (1, 1,  3, 3 ))
 
-### Alle Verbinden
+### Add into a layout together from the three lists
 for (i in 1:length (p_map)) {
   
-  ### Hintergrundfarbe
+  ### Set background color
   p1 <- p_map[[i]] + theme (plot.background = element_rect(fill="grey90", color = NA))
   p2 <- p_inset[[i]] + theme (plot.background = element_rect(fill="grey90", color = NA))
   p3 <- p_elev[[i]] + theme (plot.background = element_rect(fill="grey90", color = NA))
   
-  ### Datum als filename
+  ### Date as filename
   if (gpx_sf$double_date[i]) { add <- 2} else { add <- 1 }
   
   file <- paste ("./plots/", format (gpx_sf$date[i], "%m_%d"), "_", add, ".png", sep = "")
   
-  ### Abspeichern
+  ### Save as a png in plots
   png (file = file, width = 2300, height = 1292, bg = "grey90",
        pointsize = 6, res = 150)
   grid.arrange (p1, p2, p3, layout_matrix = lay, widths = c (1, 1, 0.6, 0.6))
@@ -249,4 +257,5 @@ for (i in 1:length (p_map)) {
   
   rm (p1, p2, p3, add)}
 
+### Save Workspace
 save.image ("./data/script_5.RData")
